87670a990011fdff444eb425c0b9a53a21771f23
Zentaro <zzzentaro@gmail.com>
2026-04-11 17:25:49 +0700

    init: wallpaper
    lemuen <3

--- gentoo-config-connect.bash
+++ gentoo-config-connect.bash
@@ -8,3 +8,5 @@ ln -sf "$here"/bash/logout.bash "$HOME"/.bash_logout
 ln -sf "$here"/alacritty "$HOME"/.config/alacritty
 ln -sf "$here"/fastfetch "$HOME"/.config/fastfetch
 ln -sf "$here"/fuzzel "$HOME"/.config/fuzzel
+mkdir -p "$HOME"/Pictures/Wallpaper
+ln -sf "$here"/Pictures/Wallpapers/lemuen-panels.png "$HOME"/Pictures/Wallpapers/lemuen-panels.png
