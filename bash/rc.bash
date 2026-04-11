23c1cffa009bb6e0058e3dccf77cf8d9e0b266c7
Zentaro <zzzentaro@gmail.com>
2026-04-11 21:48:51 +0700

    ref: remove unused aliases

--- bash/rc.bash
+++ bash/rc.bash
@@ -41,7 +41,7 @@ alias free='free -h'
 
 # image view
 iv() {
-  chafa $1 2> /dev/null || magick $1 png:- | chafa
+	chafa $1 2> /dev/null || magick $1 png:- | chafa
 }
 
 # ---
@@ -50,61 +50,61 @@ iv() {
 alias scr='brightnessctl set'
 
 pps() {
-  local option="$1"
+	local option="$1"
 
-  case "$option" in
-    1) powerprofilesctl set power-saver ;;
-    2) powerprofilesctl set balanced ;;
-    3) powerprofilesctl set performance ;;
-    *)
-      echo "usage: $FUNCNAME <option>"
-      echo "option: 1 (power-saver), 2 (balanced), 3 (performance)"
-      ;;
-  esac
-  echo "current: $(powerprofilesctl get)"
-  return
+	case "$option" in
+		1) powerprofilesctl set power-saver ;;
+		2) powerprofilesctl set balanced ;;
+		3) powerprofilesctl set performance ;;
+		*)
+			echo "usage: $FUNCNAME <option>"
+			echo "option: 1 (power-saver), 2 (balanced), 3 (performance)"
+			;;
+	esac
+	echo "current: $(powerprofilesctl get)"
+	return
 }
 
 vol() {
-  local sink='@DEFAULT_SINK@'
-  local src='@DEFAULT_SOURCE@'
+	local sink='@DEFAULT_SINK@'
+	local src='@DEFAULT_SOURCE@'
 
-  local option="$1"
-  local value="$2"
+	local option="$1"
+	local value="$2"
 
-  if [[ $option != 'in' && $option != 'out' ]]; then
-    pactl get-sink-volume "$sink" &&
-      echo "Output: $(pactl get-sink-mute $sink)" &&
-      echo &&
-      pactl get-source-volume "$src" &&
-      echo "Input: $(pactl get-source-mute $src)"
-    return
-  fi
+	if [[ $option != 'in' && $option != 'out' ]]; then
+		pactl get-sink-volume "$sink" &&
+			echo "Output: $(pactl get-sink-mute $sink)" &&
+			echo &&
+			pactl get-source-volume "$src" &&
+			echo "Input: $(pactl get-source-mute $src)"
+		return
+	fi
 
-  local target cmd_mute cmd_status
-  if [[ $option == 'out' ]]; then
-    target="$sink"
-    cmd_set='set-sink-volume'
-    cmd_mute='set-sink-mute'
-    cmd_status='get-sink-mute'
-  else
-    target="$src"
-    cmd_set='set-source-volume'
-    cmd_mute='set-source-mute'
-    cmd_status='get-source-mute'
-  fi
+	local target cmd_mute cmd_status
+	if [[ $option == 'out' ]]; then
+		target="$sink"
+		cmd_set='set-sink-volume'
+		cmd_mute='set-sink-mute'
+		cmd_status='get-sink-mute'
+	else
+		target="$src"
+		cmd_set='set-source-volume'
+		cmd_mute='set-source-mute'
+		cmd_status='get-source-mute'
+	fi
 
-  if [[ -z $value ]]; then
-    pactl "$cmd_mute" "$target" toggle
-    pactl "$cmd_status" "$target"
-    return
-  fi
+	if [[ -z $value ]]; then
+		pactl "$cmd_mute" "$target" toggle
+		pactl "$cmd_status" "$target"
+		return
+	fi
 
-  local val
-  [[ $2 =~ ^[0-9]+$ ]] && val="${2}%" || val="$2"
-  pactl "$cmd_set" "$target" "$val"
-  pactl "$cmd_status" "$target"
-  return 0
+	local val
+	[[ $2 =~ ^[0-9]+$ ]] && val="${2}%" || val="$2"
+	pactl "$cmd_set" "$target" "$val"
+	pactl "$cmd_status" "$target"
+	return 0
 }
 
 # ---
@@ -135,65 +135,63 @@ alias setsmod="sudo $EDITOR /var/lib/portage/world_sets"
 # editor
 alias er="$EDITOR"
 alias ser="sudo $EDITOR"
-alias n='nano'
-alias v='vim'
 
 qe() {
-  local item="$1"
-  local target
-  case "$item" in
-    'r')
-      target='.bashrc'
-      ;;
-    'pro')
-      target='.bash_profile'
-      ;;
-    'out')
-      target='.bash_logout'
-      ;;
-    'er')
-      target='.vimrc'
-      ;;
-    'wm')
-      target='.config/sway/config'
-      ;;
-    'run')
-      target='.config/fuzzel/fuzzel.ini'
-      ;;
-    'bar')
-      case "$2" in
-        's')
-          target='.config/waybar/style.css'
-          ;;
-        *)
-          target='.config/waybar/config.jsonc'
-          ;;
-      esac
-      ;;
-    'f')
-      case "$2" in
-        'l')
-          target='.config/fastfetch/logo'
-          ;;
-        *)
-          target='.config/fastfetch/config.jsonc'
-          ;;
-      esac
-      ;;
-    't')
-      target='.config/alacritty/alacritty.toml'
-      ;;
-    'e')
-      target='.config/yazi/yazi.toml'
-      ;;
+	local item="$1"
+	local target
+	case "$item" in
+		'r')
+			target='.bashrc'
+			;;
+		'pro')
+			target='.bash_profile'
+			;;
+		'out')
+			target='.bash_logout'
+			;;
+		'er')
+			target='.vimrc'
+			;;
+		'wm')
+			target='.config/sway/config'
+			;;
+		'run')
+			target='.config/fuzzel/fuzzel.ini'
+			;;
+		'bar')
+			case "$2" in
+				's')
+					target='.config/waybar/style.css'
+					;;
+				*)
+					target='.config/waybar/config.jsonc'
+					;;
+			esac
+			;;
+		'f')
+			case "$2" in
+				'l')
+					target='.config/fastfetch/logo'
+					;;
+				*)
+					target='.config/fastfetch/config.jsonc'
+					;;
+			esac
+			;;
+		't')
+			target='.config/alacritty/alacritty.toml'
+			;;
+		'e')
+			target='.config/yazi/yazi.toml'
+			;;
 
-    *)
-      target='.bashrc'
-      ;;
-  esac
-  "$EDITOR" "$HOME/$target"
+		*)
+			target='.bashrc'
+			;;
+	esac
+	"$EDITOR" "$HOME/$target"
 
-  return 0
+	return 0
 }
 
 # formatter
