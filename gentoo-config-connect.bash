fe6c4eff3ccb1fe7ccccc7f75f3af3cdb256a38f
Zentaro <zzzentaro@gmail.com>
2026-04-11 12:51:33 +0700

    init: alacritty config

--- gentoo-config-connect.bash
+++ gentoo-config-connect.bash
@@ -5,3 +5,4 @@ here="$(cd $(dirname "$BASH_SOURCE[0]") && pwd)"
 ln -sf "$here"/bash/profile.bash "$HOME"/.bash_profile
 ln -sf "$here"/bash/rc.bash "$HOME"/.bashrc
 ln -sf "$here"/bash/logout.bash "$HOME"/.bash_logout
+ln -sf "$here"/alacritty "$HOME"/.config/alacritty
