b500fd8c240df6d1dc945b20c04b78289c811aac
Zentaro <zzzentaro@gmail.com>
2026-04-11 21:47:51 +0700

    ref: change editor

--- bash/profile.bash
+++ bash/profile.bash
@@ -1,7 +1,7 @@
 [[ -f ~/.bashrc ]] && source ~/.bashrc
 
 export PATH="$HOME/.local/bin:$PATH"
-export EDITOR='vim'
+export EDITOR='nvim'
 mkdir -p ~/.vim/{undo,swap,backup}
 
 brightnessctl set 20%
@@ -10,6 +10,5 @@ powerprofilesctl set power-saver
 clear
 figlet "welcome home $USER!!"
 if [[ -z ${WAYLAND_DISPLAY} ]] && [[ ${XDG_VTNR} -eq 1 ]]; then
-  dbus-run-session sway --unsupported-gpu
+	dbus-run-session sway --unsupported-gpu
 fi
-
