087b5257e59db38ad8fb2c60a70c5f311bf86940
Zentaro <zzzentaro@gmail.com>
2026-04-11 17:24:27 +0700

    init: fuzzel config

--- gentoo-config-connect.bash
+++ gentoo-config-connect.bash
@@ -7,3 +7,4 @@ ln -sf "$here"/bash/rc.bash "$HOME"/.bashrc
 ln -sf "$here"/bash/logout.bash "$HOME"/.bash_logout
 ln -sf "$here"/alacritty "$HOME"/.config/alacritty
 ln -sf "$here"/fastfetch "$HOME"/.config/fastfetch
+ln -sf "$here"/fuzzel "$HOME"/.config/fuzzel
