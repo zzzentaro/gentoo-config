c2cb32d91b7eb66c69a4302b0a14cc6ab1c94a17
Zentaro <zzzentaro@gmail.com>
2026-04-11 17:52:37 +0700

    fix: delete existing symlink before replacing

--- gentoo-config-connect.bash
+++ gentoo-config-connect.bash
@@ -1,12 +1,13 @@
 #!/bin/env bash
 
-here="$(cd $(dirname "$BASH_SOURCE[0]") && pwd)"
+here="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
 ln -sf "$here"/bash/profile.bash "$HOME"/.bash_profile
 ln -sf "$here"/bash/rc.bash "$HOME"/.bashrc
 ln -sf "$here"/bash/logout.bash "$HOME"/.bash_logout
-ln -sf "$here"/alacritty "$HOME"/.config/alacritty
-ln -sf "$here"/fastfetch "$HOME"/.config/fastfetch
-ln -sf "$here"/fuzzel "$HOME"/.config/fuzzel
+for item in alacritty fastfetch fuzzel; do
+    rm -rf "$HOME/.config/$item"
+    ln -sf "$here/$item" "$HOME/.config/$item"
+done
 mkdir -p "$HOME"/Pictures/Wallpaper
 ln -sf "$here"/Pictures/Wallpapers/lemuen-panels.png "$HOME"/Pictures/Wallpapers/lemuen-panels.png
