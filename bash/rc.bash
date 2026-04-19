256fb75210f7257455526e4b0ab2bbec39d76e4a
Zentaro <zzzentaro@gmail.com>
2026-04-20 00:17:33 +0700

    ref: remove unused alias

--- bash/rc.bash
+++ bash/rc.bash
@@ -9,56 +9,51 @@ alias hex='sudo'
 # --- --- --- --- --- --- --- --- ---
 alias g='git'
 alias lg='lazygit'
-alias gc=gentoo-config
+alias gc='gentoo-config'
 
-# ---
-# for dir navigation n file management
-# ---
+# --- --- --- --- --- --- --- --- ---
+# > for dir navigation n file management
+# --- --- --- --- --- --- --- --- ---
+
+# - > explorer -
 alias er='yazi'
+
 alias cpwl='wl-copy'
 
 alias c='cd'
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias ls='eza --group-directories-first --no-permissions --octal-permissions --icons=auto --git'
+alias ls='eza --group-directories-first --no-permissions --icons=auto --git'
 alias l='ls -Ahl'
 
 alias mkdir='mkdir -p'
 alias md='mkdir'
 
 alias grep='grep --color=auto'
-alias gr='grep -i'
+alias gr='grep'
 
 alias cp='cp -i'
-
 alias rm='rm -i'
-alias rmm='sudo rm -rf'
-
 alias df='df -h'
 alias free='free -h'
 
-# image view
-iv() {
-	chafa $1 2> /dev/null || magick $1 png:- | chafa
-}
-
-# ---
-# control panel (or what could be)
-# ---
+# --- --- --- --- --- --- --- --- ---
+# > control panel (or what could be)
+# --- --- --- --- --- --- --- --- ---
 alias scr='brightnessctl set'
 
 pps() {
 	local option="$1"
 
 	case "$option" in
-		1) powerprofilesctl set power-saver ;;
-		2) powerprofilesctl set balanced ;;
-		3) powerprofilesctl set performance ;;
-		*)
-			echo "usage: $FUNCNAME <option>"
-			echo "option: 1 (power-saver), 2 (balanced), 3 (performance)"
-			;;
+	1) powerprofilesctl set power-saver ;;
+	2) powerprofilesctl set balanced ;;
+	3) powerprofilesctl set performance ;;
+	*)
+		echo "usage: $FUNCNAME <option>"
+		echo "option: 1 (power-saver), 2 (balanced), 3 (performance)"
+		;;
 	esac
 	echo "current: $(powerprofilesctl get)"
 	return
@@ -106,10 +101,9 @@ vol() {
 	return 0
 }
 
-# ---
-# gentoo's portage
-# ---
-
+# --- --- --- --- --- --- --- --- ---
+# > gentoo's portage
+# --- --- --- --- --- --- --- --- ---
 alias p='portage'
 alias psy='portage sync'
 alias p-+='portage rebuild'
@@ -124,91 +118,93 @@ alias p-='portage unmerge'
 alias pe='portage edit'
 
 alias d-c='sudo dispatch-conf'
-alias e-u='sudo etc-update'
+alias etc-u='sudo etc-update'
+alias env-u='sudo env-update'
 
 alias world='cat /var/lib/portage/world'
 alias worldmod="sudo $EDITOR /var/lib/portage/world"
 alias sets='cat /var/lib/portage/world_sets'
 alias setsmod="sudo $EDITOR /var/lib/portage/world_sets"
 
-# editor
+# --- --- --- --- --- --- --- --- ---
+# > editor
+# --- --- --- --- --- --- --- --- ---
 alias e="$EDITOR"
-alias se="sudo $EDITOR"
-
-qe() {
+alias es="sudo $EDITOR"
+eq() {
 	local item="$1"
 	local target
 	case "$item" in
-		'rc')
-			target='.bashrc'
-			;;
-		'init')
-			target='.bash_profile'
-			;;
-		'end')
-			target='.bash_logout'
-			;;
-		'e')
-			target='.vimrc'
-			;;
-		'wm')
-			target='.config/sway/config'
-			;;
-		'bar')
-			case "$2" in
-				's')
-					target='.config/waybar/style.css'
-					;;
-				*)
-					target='.config/waybar/config.jsonc'
-					;;
-			esac
-			;;
-		'run')
-			target='.config/fuzzel/fuzzel.ini'
-			;;
-		'not')
-			target='.config/mako/config'
-			;;
-		'f')
-			case "$2" in
-				'l')
-					target='.config/fastfetch/logo'
-					;;
-				*)
-					target='.config/fastfetch/config.jsonc'
-					;;
-			esac
-			;;
-		't')
-			target='.config/alacritty/alacritty.toml'
-			;;
-		'er')
-			target='.config/yazi/yazi.toml'
+	'rc')
+		target='.bashrc'
+		;;
+	'init')
+		target='.bash_profile'
+		;;
+	'end')
+		target='.bash_logout'
+		;;
+	'e')
+		target='.vimrc'
+		;;
+	'wm')
+		target='.config/sway/config'
+		;;
+	'bar')
+		case "$2" in
+		's')
+			target='.config/waybar/style.css'
 			;;
 		*)
-			target='.bashrc'
+			target='.config/waybar/config.jsonc'
 			;;
+		esac
+		;;
+	'run')
+		target='.config/fuzzel/fuzzel.ini'
+		;;
+	'not')
+		target='.config/mako/config'
+		;;
+	'f')
+		case "$2" in
+		'l')
+			target='.config/fastfetch/logo'
+			;;
+		*)
+			target='.config/fastfetch/config.jsonc'
+			;;
+		esac
+		;;
+	't')
+		target='.config/alacritty/alacritty.toml'
+		;;
+	'er')
+		target='.config/yazi/yazi.toml'
+		;;
+	*)
+		target='.bashrc'
+		;;
 	esac
 	"$EDITOR" "$HOME/$target"
 
-	return 0
+	return
 }
 
-# formatter
-alias fmtsh='shfmt -w -s -i 0 -ci -sr'
+# - > formatter -
+alias fmtsh='shfmt -w -s'
 
-# moods and flavours
-alias zidle='scr 5% && pps 1 && swaylock'
-alias zpresent='scr 10% && pps 2'
-alias zmake='scr 5% && pps 3'
-
-# waydroid
+# --- --- --- --- --- --- --- --- ---
+# > miscellaneous
+# --- --- --- --- --- --- --- --- ---
+alias ff='fastfetch'
+clear -x
+# - > waydroid -
 alias wdx='sudo -v && waydroid session stop; sleep 2 && sudo rc-service waydroid stop'
 alias wd='wdx && sudo rc-service waydroid start; sleep 2 && waydroid show-full-ui'
 
-# misc
-alias ff='fastfetch'
-clear -x
+# --- --- --- --- --- --- --- --- ---
+# > finally initialise bash
+# --- --- --- --- --- --- --- --- ---
 [[ $SHLVL -le 2 ]] && fastfetch
 eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/config.jsonc)"
