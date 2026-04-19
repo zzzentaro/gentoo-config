9a51a40a247ff34d1907233a57ae86df297fd212
Zentaro <zzzentaro@gmail.com>
2026-04-19 23:59:41 +0700

    ref: move run session down

--- bash/profile.bash
+++ bash/profile.bash
@@ -5,12 +5,10 @@ export PATH="$HOME/.local/bin:$PATH"
 
 [[ -f ~/.bashrc ]] && source ~/.bashrc && clear
 
+export EDITOR='nano'
+command -v vim >/dev/null && export EDITOR='vim' && mkdir -p ~/.vim/{undo,swap,backup}
+command -v nvim >/dev/null && export EDITOR='nvim'
+
 if [[ -z ${WAYLAND_DISPLAY} ]] && [[ ${XDG_VTNR} -eq 1 ]]; then
 	dbus-run-session sway --unsupported-gpu
 fi
-
-export EDITOR='nano'
-command -v vim > /dev/null && export EDITOR='vim'
-command -v nvim > /dev/null && export EDITOR='nvim'
-
-mkdir -p ~/.vim/{undo,swap,backup}
