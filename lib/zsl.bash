e0b589c14d6db1430bb58465fc12b01740b061f2
Zentaro <zzzentaro@gmail.com>
2026-04-20 00:42:25 +0700

    init: my shell library

---
+++ lib/zsl.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Zentaro's Shell Library
+
+zsl-error() {
+	echo "[!!] ${1:-Invalid option}" >&2
+	exit "${2:-1}"
+}
+zsl-success() {
+	echo "[ok] ${1:-Operation complete}"
+	exit 0
+}
+zsl-require-not-root() {
+	[[ $USER == 'root' ]] && zsl-error "Do not run ${0##*/} as root"
+}
+zsl-require-home() {
+	[[ -z $HOME ]] && zsl-error '$HOME is not set'
+}
+zsl-require-command() {
+	command -v "$1" >/dev/null || zsl-error "Required command not found: $1"
+}
