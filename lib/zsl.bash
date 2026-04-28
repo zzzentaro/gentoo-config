205fbca02c1cc121e8b2412d50c69f7c9a49ab2a
Zentaro <zzzentaro@gmail.com>
2026-04-28 20:19:36 +0700

    update lib

--- lib/zsl.bash
+++ lib/zsl.bash
@@ -2,18 +2,18 @@
 # Zentaro's Shell Library
 
 zsl-error() {
-	echo "[!!] ${1:-Invalid option}" >&2
+	echo "[ !! ] ${1:-Invalid option}" >&2
 	exit "${2:-1}"
 }
 zsl-success() {
-	echo "[ok] ${1:-Operation complete}"
+	echo "[ ok ] ${1:-Operation succeeded}"
 	exit 0
 }
 zsl-require-not-root() {
 	[[ $USER == 'root' ]] && zsl-error "Do not run ${0##*/} as root"
 }
 zsl-require-home() {
-	[[ -z $HOME ]] && zsl-error '$HOME is not set'
+	[[ -z "$HOME" ]] && zsl-error '$HOME is not set'
 }
 zsl-require-command() {
 	command -v "$1" >/dev/null || zsl-error "Required command not found: $1"
