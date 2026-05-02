2fd042faba4e8a54f70ce711a4038f657aaf835c
Zentaro <zzzentaro@gmail.com>
2026-05-02 20:05:51 +0700

    refactor to be posix-compliant

--- connect.sh
+++ connect.sh
@@ -1,66 +1,63 @@
 #!/bin/sh
-# This script is destructive.
+# This script is mostly for personal use and destructive
+# Back-up your own stuff <3
 set -eu
 
 is_root() { [ "$(id -u)" -eq 0 ]; }
 is_root && echo "[ !! ] Do not run as root" && exit 1
 has_home() { [ -e "$HOME" ]; }
-has_home || (echo '[ !! ] $HOME not found' && exit 1)
+has_home || { echo '[ !! ] $HOME not found' && exit 1; }
 
-HERE=$(cd "$(dirname "$0")" && pwd)
-sudo chown -R "$USER":"$USER" "$HERE"
+_HERE="$(cd "$(dirname "$0")" && pwd)"
+readonly _HERE
+sudo chown -R "$USER":"$USER" "$_HERE"
 
-# --- --- --- --- --- --- --- --- ---
-# > home directory
-# --- --- --- --- --- --- --- --- ---
-ln -sfn "$HERE"/bash/profile.bash "$HOME"/.bash_profile
-ln -sfn "$HERE"/bash/rc.bash "$HOME"/.bashrc
-ln -sfn "$HERE"/bash/logout.bash "$HOME"/.bash_logout
-ln -sfn "$HERE"/vim/rc.vim "$HOME"/.vimrc
-ln -sfn "$HERE"/.editorconfig "$HOME"/.editorconfig
+# Home directory item
+ln -sfn "$_HERE"/bash/profile.bash "$HOME"/.bash_profile
+ln -sfn "$_HERE"/bash/rc.bash "$HOME"/.bashrc
+ln -sfn "$_HERE"/bash/logout.bash "$HOME"/.bash_logout
+ln -sfn "$_HERE"/vim/rc.vim "$HOME"/.vimrc
+ln -sfn "$_HERE"/.editorconfig "$HOME"/.editorconfig
 
-rm -rf "$HOME"/db
-ln -sfn "$HERE/db" "$HOME"/db
+# Local directory program
+readonly _LOCAL_DIR="$HOME/.local"
+readonly _LIB_DIR="$_LOCAL_DIR/lib"
+readonly _STORE__LIB_DIR="$_HERE/lib"
+readonly _BIN_DIR="$_LOCAL_DIR/bin"
+readonly _STORE__BIN_DIR="$_HERE/bin"
 
-# --- --- --- --- --- --- --- --- ---
-# > dotlocal shell
-# --- --- --- --- --- --- --- --- ---
-mkdir -p "$HOME"/.local/lib
+mkdir -p "$_LIB_DIR"
 for item in zsl; do
-	chmod -x "$HERE/lib/${item}.bash"
-	rm -f "$HOME/.local/lib/$item"
-	ln -sfn "$HERE/lib/${item}.bash" "$HOME/.local/lib/$item"
+	rm -f "$_LIB_DIR/$item"
+	chmod -x "$_STORE__LIB_DIR/$item.sh"
+	ln -sfn "$_STORE__LIB_DIR/$item.sh" "$_LIB_DIR/$item"
 done
 
-mkdir -p "$HOME"/.local/bin
-for item in gentoo-config nvidia-offload portage menu noogetctl; do
-	chmod +x "$HERE/bin/${item}.sh"
-	rm -f "$HOME/.local/bin/$item"
-	ln -sfn "$HERE/bin/${item}.sh" "$HOME/.local/bin/$item"
+mkdir -p "$_BIN_DIR"
+for item in nvidia-offload portage rc-user menu; do
+	rm -f "$_BIN_DIR/$item"
+	chmod +x "$_STORE__BIN_DIR/${item}.sh"
+	ln -sfn "$_STORE__BIN_DIR/${item}.sh" "$HOME/.local/bin/$item"
 done
 
-# --- --- --- --- --- --- --- --- ---
-# > dotconfig
-# --- --- --- --- --- --- --- --- ---
-for item in hypr waybar alacritty fastfetch oh-my-posh fuzzel yazi; do
-	rm -rf "$HOME/.config/$item"
-	ln -sfn "$HERE/$item" "$HOME/.config/$item"
+# Config directory (dotfiles)
+readonly _CONFIG_DIR="$HOME/.config"
+for item in hypr alacritty waybar mako fuzzel nvim fastfetch oh-my-posh; do
+	rm -rf "${_CONFIG_DIR:?}/$item"
+	ln -sfn "$_HERE/$item" "$_CONFIG_DIR/$item"
 done
-mkdir -p "$HOME"/.config/vesktop/settings/
-rm -f "$HOME"/.config/vesktop/settings/quickCss.css
-ln -sfn "$HERE"/vesktop/settings/quickCss.css "$HOME"/.config/vesktop/settings/quickCss.css
+mkdir -p "$_CONFIG_DIR"/vesktop/settings/
+ln -sfn "$_HERE"/vesktop/settings/quickCss.css "$_CONFIG_DIR"/vesktop/settings/quickCss.css
 
-# --- --- --- --- --- --- --- --- ---
-# > xdg-user-dir PICTURES
-# --- --- --- --- --- --- --- --- ---
-mkdir -p "$HOME"/Pictures/Wallpaper
-ln -sfn "$HERE"/Pictures/Wallpapers/lemuen-panels.png "$HOME"/Pictures/Wallpapers/lemuen-panels.png
+# Wallpaper
+mkdir -p "$HOME"/Pictures/Wallpapers
+ln -sfn "$_HERE"/Pictures/Wallpapers/lemuen-panels.png "$HOME"/Pictures/Wallpapers/lemuen-panels.png
 
-# --- --- --- --- --- --- --- --- ---
-# > portage. have you mooed today?
-# --- --- --- --- --- -- --- --- ---
+# Portage, The Heart of Gentoo. Have you mooed today?
+readonly _PORTAGE_DIR="/etc/portage"
+readonly _STORE_PORTAGE_DIR="$_HERE/portage"
 command -v emerge >/dev/null || exit
 for item in make.conf repos.conf sets; do
-	sudo rm -rf /etc/portage/"$item"
-	sudo ln -sfn "$HERE/portage/$item" /etc/portage/"$item"
+	sudo rm -rf "${_PORTAGE_DIR:?}/$item"
+	sudo ln -sfn "$_STORE_PORTAGE_DIR/$item" "$_PORTAGE_DIR/$item"
 done
