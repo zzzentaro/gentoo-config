ee26d386f5fa685ab8be5a0e203fef5d944e4dd4
Zentaro <zzzentaro@gmail.com>
2026-05-04 13:41:11 +0700

    wrap var in quotes

--- bash/profile.bash
+++ bash/profile.bash
@@ -1,14 +1,11 @@
-# --- --- --- --- --- --- --- --- ---
-# > initialisation of every session
-# --- --- --- --- --- --- --- --- ---
-export PATH="$HOME/.local/bin:$PATH"
+# THIS IS A BASH PROFILE
 
-[[ -f ~/.bashrc ]] && source ~/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
+[[ -f '~/.bashrc' ]] && source ~/.bashrc
 
 export EDITOR='nano'
 command -v vim >/dev/null && export EDITOR='vim' && mkdir -p ~/.vim/{undo,swap,backup}
 command -v nvim >/dev/null && export EDITOR='nvim'
+export SUDO_EDITOR="$EDITOR"
 
-if [[ -z ${WAYLAND_DISPLAY} ]] && [[ ${XDG_VTNR} -eq 1 ]]; then
-	dbus-run-session start-hyprland
-fi
+[[ -z "$WAYLAND_DISPLAY" ]] && [[ "$XDG_VTNR" -eq 1 ]] && dbus-run-session start-hyprland
