407d0118ef0902bf79dd8f247e2a102561617764
Zentaro <zzzentaro@gmail.com>
2026-05-04 14:00:46 +0700

    refactor function to use inner emerge function

--- bin/portage.sh
+++ bin/portage.sh
@@ -1,35 +1,98 @@
 #!/bin/sh
 set -eu
-# I barely even need this anymore nowadays, lol
-
 . "$HOME"/.local/lib/zsl || {
 	echo '[!!] zsl is missing'
 	exit 1
 }
+zsl_need_command 'emerge'
+readonly _CMD="${0##*/}"
 
 readonly _PORTAGE_DIR='/etc/portage'
 
+# pretend is mostly for testing
+_PRETEND=1
+_ASK=0
+_emerge() {
+	if [ "$_PRETEND" -gt 0 ]; then
+		sudo emerge --verbose --pretend "$1"
+		return
+	fi
+
+	if [ "$_ASK" -gt 0 ]; then
+		sudo emerge --verbose --ask "$1"
+	else
+		sudo emerge --verbose "$1"
+	fi
+}
+
 _no_args_second() {
-	[ -z "$2" ] && zsl_error "Nothing to $1 with $0"
+	[ -z "${2:-}" ] && zsl_error "Nothing to $1 with $_CMD"
 }
 
+# PORTAGE SYNC
+_web_rsync() {
+	sudo rm -f /var/db/repos/gentoo/metadata/timestamp.x &&
+		sudo emerge-webrsync
+}
+_emaint_sync() {
+	sudo emaint sync --allrepos
+}
+_health_check() {
+	sudo etc-update && zsl_success "etc-update"
+	sudo env-update && zsl_success "env-update"
+	sudo dispatch-conf && zsl_success "dispatch-conf"
+	sudo emaint --fix all
+}
 portage_sync() {
-	sudo rm -f /var/db/repos/gentoo/metadata/timestamp.x
-	sudo emerge-webrsync
+	zsl_info "Starting $_CMD $1"
 	sudo -v
-	sudo emaint sync -A
+
+	for item in web_rsync emaint_sync health_check; do
+		sudo -v
+		zsl_info "Starting $item..." &&
+			_"$item" &&
+			zsl_success "$item complete!" ||
+			zsl_error "$item failed"
+	done
+	zsl_success "$_CMD $1 complete"
 }
 
+# PORTAGE REBUILD
+_revdep_rebuild() {
+	if [ "$_PRETEND" -gt 0 ]; then
+		sudo revdep-rebuild --pretend
+	else
+		sudo revdep-rebuild
+	fi
+}
+_try_revdep_rebuild() {
+	command -v revdep-rebuild >/dev/null || return 1
+
+	zsl_info "Starting revdep-rebuild..." &&
+		_revdep_rebuild &&
+		zsl_success "revdep-rebuild complete!" ||
+		zsl_error "revdep-rebuild failed"
+
+}
 portage_rebuild() {
-	sudo emerge @preserved-rebuild
-	sudo emerge @module-rebuild
-	sudo revdep-rebuild
+	zsl_info "$_CMD"
+	sudo -v
+	for item in @preserved-rebuild @module-rebuild; do
+		sudo -v
+		zsl_info "Starting $item..." &&
+			_emerge "$item" &&
+			zsl_success "$item complete!" ||
+			zsl_error "$item failed"
+	done
+	_try_revdep_rebuild
 }
 
+# #PORTAGE UPDATE
 portage_update() {
-	sudo emerge --ask --update --newuse --deep --with-bdeps=y --tree @world
+	sudo emerge --verbose --ask --update --newuse --deep --with-bdeps=y --tree @world
 }
 
+# PORTAGE CLEAN
 portage_clean() {
 	sudo emerge --ask --depclean
 	sudo -v
@@ -58,15 +121,15 @@ portage_search() {
 }
 
 portage_usedesc() {
-	if [ -z "$2" ]; then
+	if [ -z "${2:-}" ]; then
 		cat /var/db/repos/gentoo/profiles/use.desc
 	else
-		${rg:-grep} "$1" /var/db/repos/gentoo/profiles/use.desc
+		${rg:-grep} "$2" /var/db/repos/gentoo/profiles/use.desc
 	fi
 }
 
 portage_kernel() {
-	if [ -z "$2" ]; then
+	if [ -z "${2:-}" ]; then
 		zsl_info "Kernel(s) inside /usr/src/"
 		ls -Ahl /usr/src
 		echo
@@ -86,9 +149,7 @@ portage_merge() {
 	if command -v equery >/dev/null; then
 		equery u "$2"
 	fi
-	if emerge --ask --pretend --autounmask "$2"; then
-		sudo emerge "$2"
-	fi
+	sudo emerge --verbose --ask --autounmask "$2"
 }
 
 portage_unmerge() {
@@ -98,35 +159,35 @@ portage_unmerge() {
 }
 
 portage_edit() {
-	[ -z "$2" ] && sudo "$EDITOR" "$_PORTAGE_DIR/make.conf" && return
+	[ -z "${2:-}" ] && sudo "$EDITOR" "$_PORTAGE_DIR/make.conf" && return 1
 
 	_TARGET_DIR=''
-	case "$2" in
+	case "${2:-}" in
 	use) _TARGET_DIR="$_PORTAGE_DIR/package.use/" ;;
 	mask) _TARGET_DIR="$_PORTAGE_DIR/package.mask/" ;;
 	unmask) _TARGET_DIR="$_PORTAGE_DIR/package.accept_keywords/" ;;
 	sets) _TARGET_DIR="$_PORTAGE_DIR/sets/" ;;
 	env) _TARGET_DIR="$_PORTAGE_DIR/env/" ;;
-	*) echo "Usage: $0 edit [ use | unmask | sets ] [ ... ]" ;;
+	*) echo "Usage: $_CMD edit [ use | unmask | sets ] [ ... ]" ;;
 	esac
 	[ ! -d "$_TARGET_DIR" ] && sudo mkdir -p "$_TARGET_DIR"
 
-	if [ -z "$3" ]; then
+	if [ -z "${3:-}" ]; then
 		ls -Ahl "$_TARGET_DIR"
 	else
-		sudo "${EDITOR:-nano}" "$_TARGET_DIR/$3"
+		sudo "$EDITOR" "$_TARGET_DIR/$3"
 	fi
 }
 portage_help() {
 	echo "Usage:"
-	echo "   $0 < sync | rebuild | update | clean >"
-	echo "   $0 [ search | usedesc | kernel | repo ] [ ... ]"
-	echo "   $0 [ merge | unmerge | edit ] [ ... ]"
+	echo "   $_CMD < sync | rebuild | update | clean >"
+	echo "   $_CMD [ search | usedesc | kernel | repo ] [ ... ]"
+	echo "   $_CMD [ merge | unmerge | edit ] [ ... ]"
 }
 
-_cmd="portage_$1"
-if command -v "$_cmd" >/dev/null; then
-	"$_cmd" "$@"
+readonly _FINAL_CMD="${_CMD}_${1:-help}"
+if command -v "$_FINAL_CMD" >/dev/null; then
+	"$_FINAL_CMD" "$@"
 	exit 0
 else
 	portage_help
