64680a16916d6da05fe0d661f93e0f9164ee1c4f
Zentaro <zzzentaro@gmail.com>
2026-05-06 23:09:58 +0700

    add symbol for the set

--- bin/portage-sets-refresh.sh
+++ bin/portage-sets-refresh.sh
@@ -18,7 +18,7 @@ _query_sets() {
 
 _select_one_set() {
 	_SELECTED_SET="$(basename "$1")"
-	zsl_info "Current selected: $_SELECTED_SET"
+	zsl_info "Selected set: @$_SELECTED_SET"
 	sudo emerge --noreplace "@$_SELECTED_SET"
 }
 portage_sets_select() {
@@ -33,7 +33,7 @@ _is_empty_or_comment() {
 }
 _read_set_and_deselect_atom() {
 	_SELECTED_SET="$(basename "$1")"
-	zsl_info "Now deselecting atom(s) from: $_SELECTED_SET"
+	zsl_info "Deselecting atom(s) in: @$_SELECTED_SET"
 	while IFS= read -r _line; do
 		_is_empty_or_comment "$_line" && continue
 		sudo emerge --deselect "$_line"
