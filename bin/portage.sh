2c70c0305353ecc0859bdd3b345d451fecbefee0
Zentaro <zzzentaro@gmail.com>
2026-05-17 19:35:06 +0700

    script/portage: add fix command

--- bin/portage.sh
+++ bin/portage.sh
@@ -1,117 +1,51 @@
 #!/bin/sh
 set -u
-readonly _CMD="portage"
-
-. "${HOME}/.local/lib/zsl" || {
-	echo "[!!] zsl is missing"
+. "$HOME/.local/lib/zxy" || {
+	printf " !! zxy is missing\n"
 	exit 1
 }
-zsl_need_command 'emerge'
+readonly _CMD="portage"
+zxy_need_command 'sudo' 'emerge'
 
-readonly _PORTAGE_DIR='/etc/portage'
-
-_need_second_args() {
-	if [ -z "${2:-}" ]; then
-		zsl_error "Nothing to $1 with $_CMD"
-		exit 1
-	fi
-}
-_start() {
-	zsl_info "Initialise ${_CMD:-} ${1:-} ..."
-	sudo -v
-}
-# pretend is mostly for testing
-_PRETEND=0
-_ASK=0
-_emerge() {
-	if [ "$_PRETEND" -gt 0 ]; then
-		sudo emerge --verbose --pretend "$@"
-		return
-	fi
-
-	if [ "$_ASK" -gt 0 ]; then
-		sudo emerge --verbose --noreplace --autounmask --ask "$@"
-	else
-		sudo emerge --verbose --noreplace --autounmask "$@"
-	fi
-}
-
-# PORTAGE SYNC
-_web_rsync() {
-	sudo rm -f /var/db/repos/gentoo/metadata/timestamp.x &&
-		sudo emerge-webrsync
-}
-_emaint_sync() {
-	sudo emaint sync --allrepos
-}
-_health_check() {
-	sudo etc-update && zsl_success "etc-update"
-	sudo env-update && zsl_success "env-update"
-	sudo dispatch-conf && zsl_success "dispatch-conf"
-	sudo emaint --fix all
-}
+# Sync
 portage_sync() {
-	_start "$1"
-
-	_web_rsync
-	_emaint_sync
-	_health_check
-
-	zsl_success "$_CMD $1 complete"
+	sudo rm -f /var/db/repos/gentoo/metadata/timestamp.x #&& sudo emerge-webrsync
+	sudo emaint sync
 }
 
-# PORTAGE REBUILD
-_revdep_rebuild() {
-	if [ "$_PRETEND" -gt 0 ]; then
-		sudo revdep-rebuild --pretend
-	else
-		sudo revdep-rebuild
-	fi
-}
-_try_revdep_rebuild() {
-	command -v revdep-rebuild >/dev/null || return 1
-
-	zsl_info "Starting revdep-rebuild..." &&
-		_revdep_rebuild &&
-		zsl_success "revdep-rebuild complete!" ||
-		zsl_error "revdep-rebuild failed"
-
-}
+# Rebuild
 portage_rebuild() {
-	_start "$1"
-
-	for item in @preserved-rebuild @module-rebuild; do
-		sudo -v
-		zsl_info "Starting $item..." &&
-			_emerge "$item" &&
-			zsl_success "$item complete!" ||
-			zsl_error "$item failed"
+	for sets in @preserved-rebuild @module-rebuild; do
+		sudo emerge "$sets"
 	done
-	_try_revdep_rebuild
+	command -v revdep-rebuild >/dev/null 2>&1 && sudo revdep-rebuild
 }
 
-# #PORTAGE UPDATE
+# Update
 portage_update() {
-	_start "$1"
 	sudo emerge --verbose --ask --update --newuse --deep --with-bdeps=y --tree @world
 }
 
-# PORTAGE CLEAN
-portage_clean() {
-	_start "$1"
+# Fix
+portage_fix() {
+	for cmd in etc-update env-update dispatch-conf; do
+		zxy_log "$cmd"
+		sudo "$cmd"
+	done
+	sudo emaint --fix all
+}
 
+# Clean
+portage_clean() {
 	sudo emerge --ask --depclean
-	sudo -v
 	sudo eclean-dist --deep
-	sudo -v
 	sudo eclean-pkg --deep
-	sudo -v
 	sudo eclean-kernel
 }
 
-# PORTAGE FIND
+# Find
 _search() {
-	zsl_info "Entry(s) for ${1:-}"
+	zxy_log "Entry(s) for ${1:-}"
 	if command -v eix >/dev/null; then
 		EIX_LIMIT=1 eix "$1"
 	else
@@ -121,23 +55,19 @@ _search() {
 }
 _query() {
 	if command -v equery >/dev/null; then
-		zsl_info "Flag(s) for $1"
+		zxy_log "Flag(s) for $1"
 		equery u "$1"
 	fi
 }
-_find() {
+portage_find() {
 	shift
 	for package in "$@"; do
 		_search "$package"
 		_query "$package"
 	done
 }
-portage_find() {
-	_need_second_args "$@"
-	_find "$@"
-}
 
-# PORTAGE USEDESC
+# Usedesc
 portage_usedesc() {
 	_USEDESC_FILE='/var/db/repos/gentoo/profiles/use.desc'
 	if [ -z "${2:-}" ]; then
@@ -149,9 +79,10 @@ portage_usedesc() {
 	fi
 }
 
+# Kernel
 portage_kernel() {
 	if [ -z "${2:-}" ]; then
-		zsl_info "Kernel(s) inside /usr/src/"
+		zxy_log "Kernel(s) inside /usr/src/"
 		ls -Ahl /usr/src
 		echo
 	else
@@ -160,37 +91,49 @@ portage_kernel() {
 	eselect kernel list
 }
 
+# Repo
 portage_repo() {
 	eselect repository list -i
 }
 
+# Merge
 portage_merge() {
-	_need_second_args "$@"
+	if [ "$#" -lt 2 ]; then
+		portage_help
+		return 1
+	fi
 	shift
-	_ASK=1
-	_emerge "$@"
+	sudo emerge --verbose --ask "$@"
 }
 
 portage_unmerge() {
-	_need_second_args "$@"
-
-	sudo emerge --ask --deselect "$2" && sudo emerge --depclean
+	if [ "$#" -lt 2 ]; then
+		portage_help
+		return 1
+	fi
+	shift
+	sudo emerge --ask --deselect "$@" && sudo emerge --depclean
 }
 
-# PORTAGE EDIT
+# Edit
+readonly _PORTAGE_DIR='/etc/portage'
 readonly _MAKE_CONF="$_PORTAGE_DIR/make.conf"
-_edit_make_conf() {
-	sudoedit "$_MAKE_CONF" 2>/dev/null || sudo -E "$EDITOR" "$_MAKE_CONF"
+edit() {
+	"${EDITOR:-nano}" "$1" 2>/dev/null ||
+		sudoedit "$1" 2>/dev/null ||
+		sudo -E "$EDITOR" "$1"
 }
-_select_target_dir() {
+_select_dir() {
 	case "${1:-}" in
 	use) _SUB="package.use" ;;
 	mask) _SUB="package.mask" ;;
 	unmask) _SUB="package.accept_keywords" ;;
 	sets) _SUB="sets" ;;
 	env) _SUB="env" ;;
-		# TODO: refactor usage
-	*) echo "Usage: $_CMD edit [ use | unmask | sets ] [ ... ]" ;;
+	*)
+		portage_help
+		return 1
+		;;
 	esac
 	_TARGET_DIR="$_PORTAGE_DIR/${_SUB:-?}"
 	readonly _TARGET_DIR
@@ -198,11 +141,17 @@ _select_target_dir() {
 	echo "$_TARGET_DIR"
 }
 portage_edit() {
-	[ -z "${2:-}" ] && _edit_make_conf && return
-	[ -z "${3:-}" ] && ls -Ahl "$(_select_target_dir "${2:?}")/" && return 1
+	shift
 
-	readonly _ITEM="$(_select_target_dir "${2:?}")/${3:-}"
-	sudoedit "$_ITEM"
+	if [ -z "${1:-}" ]; then
+		edit "$_MAKE_CONF"
+		exit
+	elif [ -z "${2:-}" ]; then
+		ls -Ahl "$(_select_dir "$1")"
+		exit
+	fi
+
+	edit "$(_select_dir "$1")/${2:-}"
 }
 
 portage_help() {
