fb8be7f52c2169ff2bfc905b2945e2d35bff6552
Zentaro <zzzentaro@gmail.com>
2026-05-23 12:00:32 +0700

    script/nvidia-offload: export egl file, vulkan file

--- bin/nvidia-offload.sh
+++ bin/nvidia-offload.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-export LIBVA_DRIVER_NAME=nvidia
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __VK_LAYER_NV_optimus=NVIDIA_only
-export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+export __EGL_VENDOR_LIBRARY_FILENAMES='/usr/share/glvnd/egl_vendor.d/10_nvidia.json'
+export VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/nvidia_icd.json'
+export LIBVA_DRIVER_NAME='nvidia'
+export __GLX_VENDOR_LIBRARY_NAME='nvidia'
+export __VK_LAYER_NV_optimus='NVIDIA_only'
+export __NV_PRIME_RENDER_OFFLOAD_PROVIDER='NVIDIA-G0'
 export __NV_PRIME_RENDER_OFFLOAD=1
 
 exec "$@"
