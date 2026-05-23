7116b9afe4a77810d3ae122955c4bb625d4a7503
Zentaro <zzzentaro@gmail.com>
2026-05-23 13:10:55 +0700

    config/hyprland/theme/garnet: fix returned variables

--- hypr/theme-garnet.lua
+++ hypr/theme-garnet.lua
@@ -1,28 +1,29 @@
 -- source: rosepinetheme.com
 
-local base = 0x191724
-local surface = 0x1f1d2e
-local overlay = 0x26233a
-local muted = 0x6e6a86
-local subtle = 0x908caa
-local text = 0xe0def4
-local love = 0xeb6f92
-local gold = 0xf6c177
-local rose = 0xebbcba
-local pine = 0x31748f
-local foam = 0x9ccfd8
-local iris = 0xc4a7e7
-local highlightLow = 0x21202e
-local highlightMed = 0x403d52
-local highlightHigh = 0x524f67
+local base = "191724"
+local surface = "1f1d2e"
+local overlay = "26233a"
+local muted = "6e6a86"
+local subtle = "908caa"
+local text = "e0def4"
+local love = "eb6f92"
+local gold = "f6c177"
+local rose = "ebbcba"
+local pine = "31748f"
+local foam = "9ccfd8"
+local iris = "c4a7e7"
+local highlightLow = "21202e"
+local highlightMed = "403d52"
+local highlightHigh = "524f67"
 
 local THEME = {
-	primary = love,
-	primaryAlpha = "eb6f92",
-	secondary = gold,
-	tertiary = iris,
+	primary = "rgb(" .. love .. ")",
+	primaryAlpha = love,
+	secondary = "rgb(" .. gold .. ")",
+	tertiary = "rgb(" .. iris .. ")",
 }
 
+hl.exec_cmd("hyprctl hyprpaper wallpaper ,~/Pictures/Wallpapers/kanaria.png")
 hl.env("XCURSOR_THEME", "BreezeX-RosePineDawn")
 hl.env("XCURSOR_SIZE", 48)
 hl.env("HYPRCURSOR_THEME", "rose-pine-dawn-hyprcursor")
