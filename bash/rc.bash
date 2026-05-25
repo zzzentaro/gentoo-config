4fbb8bef9c85b0573d26754fdaed26a4be0f7292
Zentaro <zzzentaro@gmail.com>
2026-05-25 08:34:11 +0700

    bashrc: add alias for checking gpu without waking up

--- bash/rc.bash
+++ bash/rc.bash
@@ -124,6 +124,8 @@ alias ff='fastfetch'
 alias wd-stop='sudo waydroid session stop && sudo rc-service waydroid stop'
 alias wd-start='wd-stop && sudo rc-service waydroid start && waydroid show-full-ui'
 
+alias nvidia-status='cat /sys/bus/pci/devices/0000\:01\:00.0/power/runtime_status'
+
 # Finally, start interactive shell
 eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/config.jsonc)"
 clear -x
