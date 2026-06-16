6a38b05b3b7e0ee06ff396638a849826a2274652
Zentaro <zzzentaro@gmail.com>
2026-06-16 15:24:10 +0700

    mount-git: remove script

--- home/zentaro/config/bash/functions/mount-git.bash
+++
@@ -1,7 +0,0 @@
-mount-git() {
-	_git_partition=/dev/nvme0n1p6
-	_git_home="$HOME/git"
-	mkdir -p -- "$_git_home"
-	sudo mount -- "$_git_partition" "$_git_home"
-	sudo chown -R "$USER": "$_git_home"
-}
