a4a945917f90807f4be9f8a2d13124c772f77427
Zentaro <zzzentaro@gmail.com>
2026-07-02 21:44:28 +0700

    hyprland: reorganize

--- home/zentaro/config/hypr/hyprland.lua
+++ home/zentaro/config/hypr/hyprland.lua
@@ -1,6 +1,9 @@
--- HYPRLAND CONFIG
+-- hyprland.lua
 
--- Connected device
+--------------------
+---- MY DEVICES ----
+--------------------
+
 hl.monitor({
 	output = "",
 	mode = "preferred",
@@ -18,34 +21,32 @@ hl.config({
 	},
 })
 
--- Start hyprland
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
 hl.on("hyprland.start", function()
 	hl.exec_cmd("gentoo-pipewire-launcher restart &")
-	hl.exec_cmd(
-		"waybar & hyprpaper & mako & wl-clip-persist --clipboard regular &"
-	)
+	hl.exec_cmd("waybar & hyprpaper & mako &")
+	hl.exec_cmd("wl-clip-persist --clipboard regular &")
 	hl.exec_cmd("brightnessctl set 50%")
 end)
+
+-- Import the theme!
 local theme = require("./theme")
 
--- Export graphics variables
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-
--- Export toolkit backed variables
-hl.env("GDK_BACKEND", "wayland,x11,*")
-hl.env("QT_QPA_PLATFORM", "wayland;xcb")
-hl.env("SDL_VIDEODRIVER", "wayland")
-hl.env("CLUTTER_BACKEND", "wayland")
-
--- Bind
+---------------------
+---- KEYBINDINGS ----
+---------------------
 local MOD = "SUPER"
 
+-- Switch window
 hl.bind(MOD .. " + h", hl.dsp.focus({ direction = "left" }))
 hl.bind(MOD .. " + j", hl.dsp.focus({ direction = "down" }))
 hl.bind(MOD .. " + k", hl.dsp.focus({ direction = "up" }))
 hl.bind(MOD .. " + l", hl.dsp.focus({ direction = "right" }))
 
--- Move tiled window
+-- Move floating window
 hl.bind(MOD .. " + SHIFT + h", hl.dsp.window.move({ direction = "left" }))
 hl.bind(MOD .. " + SHIFT + j", hl.dsp.window.move({ direction = "down" }))
 hl.bind(MOD .. " + SHIFT + k", hl.dsp.window.move({ direction = "up" }))
@@ -73,6 +74,7 @@ hl.bind(
 	{ repeating = true }
 )
 
+-- Workspaces
 hl.bind(MOD .. " + 1", hl.dsp.focus({ workspace = 1 }))
 hl.bind(MOD .. " + 2", hl.dsp.focus({ workspace = 2 }))
 hl.bind(MOD .. " + 3", hl.dsp.focus({ workspace = 3 }))
@@ -95,7 +97,7 @@ hl.bind(MOD .. " + SHIFT + 8", hl.dsp.window.move({ workspace = 8 }))
 hl.bind(MOD .. " + SHIFT + 9", hl.dsp.window.move({ workspace = 9 }))
 hl.bind(MOD .. " + SHIFT + 0", hl.dsp.window.move({ workspace = 10 }))
 
--- Fn
+-- Function keys
 hl.bind(
 	"XF86AudioRaiseVolume",
 	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
@@ -144,8 +146,8 @@ hl.bind(
 	{ locked = true }
 )
 
+-- Death and creation
 hl.bind(MOD .. " + CTRL + Q", hl.dsp.window.close())
-
 hl.bind(MOD .. " + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(MOD .. " + CTRL + F", hl.dsp.window.cycle_next({ direction = "down" }))
 hl.bind(MOD .. " + SHIFT + F", hl.dsp.window.fullscreen(0))
@@ -156,15 +158,16 @@ hl.bind(MOD .. " + ALT + Q", hl.dsp.exec_cmd("hyprexit"))
 local terminal = "alacritty"
 hl.bind(MOD .. " + Return", hl.dsp.exec_cmd(terminal))
 
-local term_float = terminal .. " -T floatty"
+local terminal_floating = terminal .. " -T floatty"
+hl.bind(MOD .. " + ALT + Return", hl.dsp.exec_cmd(terminal_floating))
 hl.on("hyprland.start", function()
-	hl.exec_cmd(term_float)
+	hl.exec_cmd(terminal_floating)
 end)
 
-local fileManager = "thunar"
-hl.bind(MOD .. " + E", hl.dsp.exec_cmd(fileManager))
+local file_manger = "thunar"
+hl.bind(MOD .. " + E", hl.dsp.exec_cmd(file_manger))
 
-local browser = "firefox-bin"
+local browser = "firefox"
 hl.bind(MOD .. " + B", hl.dsp.exec_cmd(browser))
 
 local launcher = "fuzzel"
@@ -185,7 +188,7 @@ hl.bind(
 	hl.dsp.exec_cmd('grim -g "$(slurp)" - | swappy -f - -o ' .. psf)
 )
 
--- Submap: reize
+-- Submap: resize
 hl.bind(MOD .. " + R", hl.dsp.submap("resize"))
 hl.define_submap("resize", function()
 	hl.bind(
@@ -211,12 +214,25 @@ hl.define_submap("resize", function()
 	hl.bind("escape", hl.dsp.submap("reset"))
 end)
 
--- Appearance
+hl.gesture({
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
+})
+
+-----------------------
+---- LOOK AND FEEL ----
+-----------------------
 hl.config({
+	dwindle = {
+		preserve_split = true,
+	},
+
 	general = {
-		layout = "scrolling",
-		gaps_in = 0,
-		gaps_out = 0,
+		layout = "dwindle",
+
+		gaps_in = 4,
+		gaps_out = 8,
 		border_size = 2,
 
 		col = {
@@ -226,29 +242,28 @@ hl.config({
 
 		resize_on_border = false,
 
-		allow_tearing = true,
-	},
-
-	scrolling = {
-		fullscreen_on_one_column = true,
-		focus_fit_method = 1,
-		column_width = 0.9,
-		direction = "down",
+		allow_tearing = false,
 	},
 
 	decoration = {
-		rounding = 0,
+		rounding = 4,
 		rounding_power = 2,
 
 		active_opacity = 1.0,
-		inactive_opacity = 1.0,
+		inactive_opacity = 0.9,
 
 		shadow = {
-			enabled = false,
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = theme.muted,
 		},
 
 		blur = {
-			enabled = false,
+			enabled = true,
+			size = 0,
+			passes = 4,
+			vibrancy = 0,
 		},
 	},
 
@@ -257,82 +272,174 @@ hl.config({
 	},
 
 	misc = {
-		force_default_wallpaper = 0,
+		force_default_wallpaper = -1,
 		disable_hyprland_logo = true,
 	},
 })
 
+-- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+hl.curve(
+	"easeOutQuint",
+	{ type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } }
+)
+hl.curve(
+	"easeInOutCubic",
+	{ type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } }
+)
 hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
-hl.curve("md3_standard", { type = "bezier", points = { { 0.2, 0 }, { 0, 1 } } })
 hl.curve(
-	"md3_decel",
-	{ type = "bezier", points = { { 0.05, 0.7 }, { 0.1, 1 } } }
+	"almostLinear",
+	{ type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } }
 )
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
+
+-- Default springs
 hl.curve(
-	"md3_accel",
-	{ type = "bezier", points = { { 0.3, 0 }, { 0.8, 0.15 } } }
-)
-hl.curve(
-	"overshot",
-	{ type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.1 } } }
-)
-hl.curve(
-	"crazyshot",
-	{ type = "bezier", points = { { 0.1, 1.5 }, { 0.76, 0.92 } } }
-)
-hl.curve(
-	"hyprnostretch",
-	{ type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.0 } } }
-)
-hl.curve("fluent_decel", { type = "bezier", points = { { 0.1, 1 }, { 0, 1 } } })
-hl.curve(
-	"easeInOutCirc",
-	{ type = "bezier", points = { { 0.85, 0 }, { 0.15, 1 } } }
-)
-hl.curve(
-	"easeOutCirc",
-	{ type = "bezier", points = { { 0, 0.55 }, { 0.45, 1 } } }
-)
-hl.curve(
-	"easeOutExpo",
-	{ type = "bezier", points = { { 0.16, 1 }, { 0.3, 1 } } }
+	"easy",
+	{ type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 }
 )
 
 hl.animation({
+	leaf = "global",
+	enabled = true,
+	speed = 10,
+	bezier = "default",
+})
+hl.animation({
+	leaf = "border",
+	enabled = true,
+	speed = 5.39,
+	bezier = "easeOutQuint",
+})
+hl.animation({
 	leaf = "windows",
 	enabled = true,
-	speed = 3,
-	bezier = "md3_decel",
-	style = "popin 60%",
+	speed = 4.79,
+	spring = "easy",
 })
-hl.animation({ leaf = "border", enabled = true, speed = 10, bezier = "default" })
+hl.animation({
+	leaf = "windowsIn",
+	enabled = true,
+	speed = 4.1,
+	spring = "easy",
+	style = "popin 87%",
+})
+hl.animation({
+	leaf = "windowsOut",
+	enabled = true,
+	speed = 1.49,
+	bezier = "linear",
+	style = "popin 87%",
+})
+hl.animation({
+	leaf = "fadeIn",
+	enabled = true,
+	speed = 1.73,
+	bezier = "almostLinear",
+})
+hl.animation({
+	leaf = "fadeOut",
+	enabled = true,
+	speed = 1.46,
+	bezier = "almostLinear",
+})
 hl.animation({
 	leaf = "fade",
 	enabled = true,
-	speed = 2.5,
-	bezier = "md3_decel",
+	speed = 3.03,
+	bezier = "quick",
+})
+hl.animation({
+	leaf = "layers",
+	enabled = true,
+	speed = 3.81,
+	bezier = "easeOutQuint",
+})
+hl.animation({
+	leaf = "layersIn",
+	enabled = true,
+	speed = 4,
+	bezier = "easeOutQuint",
+	style = "fade",
+})
+hl.animation({
+	leaf = "layersOut",
+	enabled = true,
+	speed = 1.5,
+	bezier = "linear",
+	style = "fade",
+})
+hl.animation({
+	leaf = "fadeLayersIn",
+	enabled = true,
+	speed = 1.79,
+	bezier = "almostLinear",
+})
+hl.animation({
+	leaf = "fadeLayersOut",
+	enabled = true,
+	speed = 1.39,
+	bezier = "almostLinear",
 })
 hl.animation({
 	leaf = "workspaces",
 	enabled = true,
-	speed = 3.5,
-	bezier = "easeOutExpo",
-	style = "slide",
+	speed = 1.94,
+	bezier = "almostLinear",
+	style = "fade",
 })
 hl.animation({
-	leaf = "specialWorkspace",
+	leaf = "workspacesIn",
 	enabled = true,
-	speed = 3,
-	bezier = "md3_decel",
-	style = "slidevert",
+	speed = 1.21,
+	bezier = "almostLinear",
+	style = "fade",
+})
+hl.animation({
+	leaf = "workspacesOut",
+	enabled = true,
+	speed = 1.94,
+	bezier = "almostLinear",
+	style = "fade",
+})
+hl.animation({
+	leaf = "zoomFactor",
+	enabled = true,
+	speed = 7,
+	bezier = "quick",
 })
 
--- Rule (latest takes priority)
+-- "Smart gaps" / "No gaps when only"
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
 hl.window_rule({
+	name = "no-gaps-wtv1",
+	match = { float = false, workspace = "w[tv1]" },
+	border_size = 0,
+	rounding = 0,
+})
+hl.window_rule({
+	name = "no-gaps-f1",
+	match = { float = false, workspace = "f[1]" },
+	border_size = 0,
+	rounding = 0,
+})
+
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+local suppressMaximizeRule = hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
 	name = "suppress-maximize-events",
 	match = { class = ".*" },
+
 	suppress_event = "maximize",
 })
+suppressMaximizeRule:set_enabled(true)
 
 hl.window_rule({
 	name = "fix-xwayland-drags",
@@ -354,18 +461,17 @@ hl.window_rule({
 	float = true,
 })
 
+-- Start of my own config
+
 hl.window_rule({
-	name = "floatty",
 	match = { title = "floatty" },
-	float = false,
-	--move = "12 48",
-	--size = "1400 1020",
-	workspace = "3",
+	float = true,
+	size = "1280 720",
+	move = "540 260",
 })
 
 hl.window_rule({
-	name = "browser",
-	match = { class = "firefox-bin" },
+	match = { class = "firefox-esr" },
 	workspace = "2 silent",
 })
 
@@ -378,10 +484,18 @@ hl.window_rule({
 })
 hl.window_rule({ match = { class = "osu!" }, workspace = "9 silent" })
 hl.window_rule({
-	name = "endfield",
+	match = { class = "steam_app_0" },
+	workspace = "9 silent",
+	fullscreen = false,
+})
+hl.window_rule({
 	match = { title = "Endfield" },
 	workspace = "9 silent",
-	rounding = 0,
+	fullscreen = true,
+})
+hl.window_rule({
+	match = { title = "Genshin Impact" },
+	workspace = "9 silent",
 	fullscreen = true,
 })
 
@@ -394,6 +508,10 @@ hl.window_rule({
 	workspace = "10 silent",
 })
 hl.window_rule({
-	match = { title = "Twintail Launcher" },
+	match = { class = "moe.launcher.an-anime-game-launcher" },
+	workspace = "10 silent",
+})
+hl.window_rule({
+	match = { title = "Heroic Games Launcher" },
 	workspace = "10 silent",
 })
