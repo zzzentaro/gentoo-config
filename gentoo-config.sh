b5c2fc637d3be4d5a0a8e693c8227939ca3cd401
Zentaro <zzzentaro@gmail.com>
2026-07-07 08:40:34 +0700

    script/gentoo-config: rename function

--- gentoo-config.sh
+++ gentoo-config.sh
@@ -11,7 +11,7 @@ mkdir -p -- "$backup_dir"
 
 readonly repo_home="$HERE/home/zentaro"
 
-linkin()
+symlink()
 {
 	echo "mv -- "$2" "$backup_dir/$(basename "$2")-$(date +%F)-$(date +%s)" || true"
 	mv -- "$2" "$backup_dir/$(basename "$2")-$(date +%F)-$(date +%s)" || true
@@ -21,25 +21,25 @@ linkin()
 }
 
 ## Config
-echo 'linkin config'
+echo 'symlink config'
 readonly home_config="$HOME/.config" repo_home_config="$repo_home/config"
 mkdir -p -- "$home_config"
 
 for config in "$repo_home_config/"*; do
-	linkin "$config" "$home_config/$(basename -- "$config")"
+	symlink "$config" "$home_config/$(basename -- "$config")"
 done
 
 ## Bash is special
-echo 'linkin bash'
+echo 'symlink bash'
 readonly repo_bash="$repo_home_config/bash" home_bash="$HOME"
 
-linkin "$repo_bash/profile.bash" "$home_bash/.bash_profile"
-linkin "$repo_bash/bashrc.bash" "$home_bash/.bashrc"
-linkin "$repo_bash/logout.bash" "$home_bash/.bash_logout"
-linkin "$repo_bash/functions" "$home_bash/.bashrc.d"
+symlink "$repo_bash/profile.bash" "$home_bash/.bash_profile"
+symlink "$repo_bash/bashrc.bash" "$home_bash/.bashrc"
+symlink "$repo_bash/logout.bash" "$home_bash/.bash_logout"
+symlink "$repo_bash/functions" "$home_bash/.bashrc.d"
 
 ## Local
-echo 'linkin local'
+echo 'symlink local'
 readonly repo_home_lib="$repo_home/local/lib" home_lib="$HOME/.local/lib"
 mkdir -p -- "$home_lib"
 readonly repo_home_bin="$repo_home/local/bin" home_bin="$HOME/.local/bin"
@@ -47,23 +47,25 @@ mkdir -p -- "$home_bin"
 
 for bin in "$repo_home_bin/"*; do
 	chmod +x "$bin"
-	linkin "$bin" "$home_bin/$(basename -- "$bin")"
+	symlink "$bin" "$home_bin/$(basename -- "$bin")"
 done
 
-echo 'linkin wal'
+echo 'symlink wal'
 readonly repo_wal="$HERE/usr/share/wallpapers" home_wal="$HOME/Pictures/Wallpapers"
 mkdir -p -- "$home_wal"
 
 for wal in "$repo_wal/"*; do
-	linkin "$wal" "$home_wal/$(basename -- "$wal")"
+	symlink "$wal" "$home_wal/$(basename -- "$wal")"
 done
+
+## Root
 
 command -v emerge >/dev/null 2>&1 || exit
 
-echo 'linkin portage'
+echo 'symlink portage'
 readonly repo_portage="$HERE/etc/portage" host_portage='/etc/portage'
 [ ! -d "$host_portage" ] && sudo mkdir -p -- "$host_portage"
 
 for item in "$repo_portage/"*; do
-	linkin "$item" "$host_portage/$(basename -- "$item")"
+	symlink "$item" "$host_portage/$(basename -- "$item")"
 done
